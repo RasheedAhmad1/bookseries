ea5887eb61d8444b945ad481afad7d7179094a50
rasheedahmad1 <rasheedahmad22333@gmail.com>
2024-10-25 14:06:21 -0700

    Home module linked in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,17 +7,30 @@
 @endpush
 
 @push('content')
-      <!-- Examples -->
-      <div class="row mb-5">
-        <div class="col-lg-3 col-md-6 col-md-12 mb-4">
-            <a href="books" class="btn">
+    <!-- Modules -->
+    <div class="row mb-5">
+        <!-- Book Module -->
+        <div class="col-md-6 col-xl-4">
+            <a href="books">
                 <div class="card h-100">
                     <div class="card-body">
-                        <h5 class="card-title">Books</h5>
-                        <p class="card-text">Books library</p>
+                        <h5 class="card-title">Book</h5>
+                        <p class="card-text">Books Library</p>
                     </div>
                 </div>
             </a>
-        </div>
-    </div>
+        </div> <!--/ Book Module -->
+
+        <!-- Setting Module -->
+        <div class="col-md-6 col-xl-4">
+            <a href="setting">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="card-title">Setting</h5>
+                        <p class="card-text">Get settings</p>
+                    </div>
+                </div>
+            </a>
+        </div> <!--/ Setting Module -->
+    </div> <!--/ Modules -->
 @endpush
