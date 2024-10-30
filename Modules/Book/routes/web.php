f89b895473c138d013073105ab754a3be991c358
Ammar-gith <aammarulhasan094@gmail.com>
2024-10-30 13:02:08 +0500

    delete routes created

--- Modules/Book/routes/web.php
+++ Modules/Book/routes/web.php
@@ -11,18 +11,19 @@ use Modules\Book\App\Http\Controllers\AuthorController;
 //     });
 // });
 
+// Book routes
+Route::get('book/dashboard', [BookController::class, 'index'])->name('book.dashboard');
+Route::get('books', [BookController::class, 'showBooks'])->name('books.show');
+Route::get('addbook', [BookController::class, 'create'])->name('book.create');
+Route::post('storebook', [BookController::class, 'store'])->name('book.store');
+Route::get('editbook/{id}', [BookController::class, 'edit'])->name('book.edit');
+Route::post('updatebook/{id}', [BookController::class, 'update'])->name('book.update');
+Route::delete('/delete-book/{id}', [BookController::class, 'destroy'])->name('delete.book');
+
 // Author routes
 Route::get('authors', [AuthorController::class, 'index'])->name('authors.show');
 Route::get('addauthor', [AuthorController::class, 'create'])->name('author.create');
 Route::post('storeauthor', [AuthorController::class, 'store'])->name('author.store');
 Route::get('editauthor/{id}', [AuthorController::class, 'edit'])->name('author.edit');
 Route::post('updateauthor/{id}', [AuthorController::class, 'update'])->name('author.update');
-Route::get('deleteauthor/{id}', [AuthorController::class, 'destroy'])->name('author.delete');
-
-// Book routes
-Route::get('books', [BookController::class, 'index'])->name('books.show');
-Route::get('addbook', [BookController::class, 'create'])->name('book.create');
-Route::post('storebook', [BookController::class, 'store'])->name('book.store');
-Route::get('editbook/{id}', [BookController::class, 'edit'])->name('book.edit');
-Route::post('updatebook/{id}', [BookController::class, 'update'])->name('book.update');
-Route::get('deletebook/{id}', [BookController::class, 'destroy'])->name('book.delete');
+Route::delete('/delete-author/{id}', [AuthorController::class, 'destroy'])->name('author.delete');
