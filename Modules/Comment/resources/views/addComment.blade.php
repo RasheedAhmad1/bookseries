72886c8d5c1cf8114138812c8e217430625a30cc
rasheedahmad1 <rasheedahmad22333@gmail.com>
2024-10-31 13:54:24 -0700

    Comment module created

---
+++ Modules/Comment/resources/views/addComment.blade.php
@@ -0,0 +1,83 @@
+@extends('masterLayout.master')
+@push('content')
+    <!-- Dynamic Breadcrumb -->
+    {{-- <div class="row">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                @foreach ($breadcrumbs as $breadcrumb)
+                    @if (!$loop->last)
+                        <!-- Linkes for items-->
+                        <li class="breadcrumb-item anchor-link">
+                            <a href="{{ $breadcrumb['url'] }}">{{ $breadcrumb['name'] }}</a>
+                        </li>
+                    @else
+                        <!-- Active last item but not a link -->
+                        <li class="breadcrumb-item active text-primary" aria-current="page">{{ $breadcrumb['name'] }}</li>
+                    @endif
+                @endforeach
+            </ol>
+        </nav>
+    </div><!--/ Dynamic Breadcrumb --> --}}
+
+    <!--Form -->
+    <div class="row">
+        <div class="col-xxl">
+            <div class="card mb-4">
+                <div class="card-header col-md-12 d-flex justify-content-between align-items-center">
+                    <h5 class="card-header text-primary">New Comment</h5>
+                </div>
+                <hr class="my-4 mx-n4">
+                <form action="{{ route('comment.store') }}" method="POST" class="card-body" enctype="multipart/form-data">
+                    @csrf
+
+                    {{-- Name --}}
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label text-sm-end" for="name">Name</label>
+                        <div class="col-sm-8">
+                            <input type="text" name="name" id="name" class="form-control"
+                                placeholder="Enter name" />
+                        </div>
+                    </div>
+
+                    {{-- Comment --}}
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label text-sm-end" for="comment">Comment</label>
+                        <div class="col-sm-8">
+                            <textarea type="text" name="comment" id="editor" class="editor form-control"
+                                placeholder="Enter comment"></textarea>
+                        </div>
+                    </div>
+
+                    {{-- Comment Status --}}
+                    <div class="row mb-3">
+                        <label for="status" class="col-sm-2 col-form-label text-sm-end">Comment Status</label>
+                        <div class="col-sm-8">
+                            <select id="status" name="status" class="form-select">
+                                <option value="">Select status</option>
+                                @foreach ($statuses as $key => $value)
+                                    <option value="{{ $key }}">
+                                        {{ $value }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                    <hr class="my-4 mx-n4">
+
+                    {{-- Add Comment button --}}
+                    <div class="card-footer col-md-12 d-flex justify-content-center align-items-center">
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endpush
+
+@push('scripts')
+    <script src="{{ asset('assets/editors/ckeditor/ckeditor.js') }}"></script>
+    <script>
+        CKEDITOR.replace('editor');
+    </script>
+@endpush
