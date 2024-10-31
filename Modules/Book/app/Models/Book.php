5a3b0d5ff5a47c250165859ead9d4e1966023eed
rasheedahmad1 <rasheedahmad22333@gmail.com>
2024-10-31 13:53:11 -0700

    Book module updated

--- Modules/Book/app/Models/Book.php
+++ Modules/Book/app/Models/Book.php
@@ -3,19 +3,13 @@
 namespace Modules\Book\App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Modules\Book\Database\Factories\BookFactory;
-// use Modules\Book\App\Models\Author;
+use Modules\Author\App\Models\Author;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\HasMedia;
 
 class Book extends Model implements HasMedia
 {
-    // Optionally specify the table name, if it differs from the pluralized form of the model name
-    // protected $table = 'users';
-
-
-    use HasFactory, InteractsWithMedia;
+    use InteractsWithMedia;
 
     public const statuses = [
         1 => 'Available',
@@ -26,16 +20,7 @@ class Book extends Model implements HasMedia
 
     ];
 
-    // Define the fillable fields (mass assignment protection) | attributes that are mass assignable
     protected $fillable = ['title', 'description', 'publisher', 'language', 'orderNo', 'status', 'price', 'online_amount', 'ship_amount', 'author_id'];
-
-    // Allow all fields except 'is_admin' and 'id' to be mass assigned
-    // protected $guarded = ['is_admin', 'id'];
-
-    // protected static function newFactory(): BookFactory
-    // {
-    //     // return BookFactory::new();
-    // }
 
     public function author()
     {
