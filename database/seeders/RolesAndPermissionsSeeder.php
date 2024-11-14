91e29bd6b95bacecc044af74827abb66d01abc93
rasheedahmad1 <rasheedahmad22333@gmail.com>
2024-11-14 15:35:20 +0500

    Roles and Permissions Seeder updated accordingly

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -9,22 +9,10 @@ use Spatie\Permission\Models\Role;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    // Run the database seeds
     public function run(): void
     {
         // Create permissions if they doesn't already exist
-        Permission::firstOrCreate(['name' => 'edit books', 'guard_name' => 'web']);
-        Permission::firstOrCreate(['name' => 'delete books', 'guard_name' => 'web']);
-        Permission::firstOrCreate(['name' => 'publish books', 'guard_name' => 'web']);
-
-        // Create roles if they doesn't already exist
-        $writerRole = Role::firstOrCreate(['name' => 'writer', 'guard_name' => 'web']);
-        $adminRole = Role::firstOrCreate(['name' => 'admin', 'guard_name' => 'web']);
-
-        // Assign permissions to roles 
-        $writerRole->givePermissionTo('edit books');
-        $adminRole->givePermissionTo(['edit books', 'delete books', 'publish books']);
+        Permission::firstOrCreate(['name' => 'edit_books', 'guard_name' => 'web']);
     }
 }
