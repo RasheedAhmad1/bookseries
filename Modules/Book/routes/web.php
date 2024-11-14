659828aedcd3917ac09e1800c278ce23f17a0321
rasheedahmad1 <rasheedahmad22333@gmail.com>
2024-11-14 15:33:06 +0500

    Show single book "route" created

--- Modules/Book/routes/web.php
+++ Modules/Book/routes/web.php
@@ -13,6 +13,7 @@ Route::get('books', [BookController::class, 'index'])->name('books.index');
 Route::get('create', [BookController::class, 'create'])->name('book.create');
 Route::post('storebook', [BookController::class, 'store'])->name('book.store');
 Route::get('editbook/{id}', [BookController::class, 'edit'])->name('book.edit');
+Route::get('showbook/{id}', [BookController::class, 'show'])->name('book.show');
 Route::post('updatebook/{id}', [BookController::class, 'update'])->name('book.update');
 Route::get('deletebook/{id}', [BookController::class, 'destroy'])->name('book.delete');
 
