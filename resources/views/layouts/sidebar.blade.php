696e1c4196b6dd49953ca3738236c7e310ff626b
Ammar-gith <aammarulhasan094@gmail.com>
2024-12-09 10:39:35 +0500

    comment permission in sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -33,14 +33,14 @@
             </a>
             <ul class="menu-sub">
                 {{-- @dd(auth()->user()->hasPermissionTo('book_index')) --}}
-                @can('book_index')
+                {{-- @can('book_index') --}}
                     <li
                         class="menu-item {{ in_array(request()->path(), ['books', 'create', 'editbook/' . request('id'), 'deletebook/' . request('id')]) ? 'active' : '' }}">
                         <a href="{{ route('books.index') }}" class="menu-link">
                             <div data-i18n="Books">Books</div>
                         </a>
                     </li>
-                @endcan
+                {{-- @endcan --}}
                 <li
                     class="menu-item {{ in_array(request()->path(), ['tests', 'addtest', 'edittest/' . request('id'), 'deletetest/' . request('id')]) ? 'active' : '' }}">
                     <a href="{{ route('tests.show') }}" class="menu-link">
